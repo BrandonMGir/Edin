9228c58f825390664463073a09fcc8eac6f65f7b
Brandon Girard <branmic11@gmail.com>
2025-02-10 22:34:51 -0500

    Setup account page.

--- force-app/main/default/lwc/pages/lwc/account/account.js
+++ force-app/main/default/lwc/pages/lwc/account/account.js
@@ -1,3 +1,17 @@
 import { LightningElement } from 'lwc';
+import DUMMY_PROPERTY from '@salesforce/resourceUrl/DummyProperty';
 
-export default class Account extends LightningElement {}
+export default class Account extends LightningElement {
+
+    profileimage = DUMMY_PROPERTY;
+    name = 'John Doe';
+    email = 'jdoe123@gmail.com';
+
+    favoritedproperties = [
+        {name: 'property 1'},
+        {name: 'property 2'},
+        {name: 'property 3'},
+        {name: 'property 4'},
+        {name: 'property 5'},
+    ];
+}
